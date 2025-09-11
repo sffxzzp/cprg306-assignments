94805d4a9ec70b9cb9b36e351c04997bce4aa1b0
sffxzzp <sffxzzp@126.com>
2025-09-11 15:45:56 -0600

    week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+	<section>
+	  <p>Name: Zhiping</p>
+	  <p>Github: <Link href="https://github.com/sffxzzp/cprg306-assignments">sffxzzp/cprg306-assignments</Link></p>
+	</section>
+  );
+}
