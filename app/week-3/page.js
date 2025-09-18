d8c8b47f07bda457217d4f9593eb418de676059a
sffxzzp <sffxzzp@126.com>
2025-09-18 17:29:08 -0600

    a nicer look for week 3

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,8 +2,8 @@ import ItemList from './item-list';
 
 export default function Page() {
 	return (
-		<main>
-			<h1>Shopping List</h1>
+		<main className="mx-auto my-4 p-4 max-w-2xl flex flex-col justify-center">
+			<h1 className="mb-4 text-2xl font-bold text-center">Shopping List</h1>
 			<ItemList />
 		</main>
 	);
