5a11f870bea3f231e7a9f01cccee00108545b84c
sffxzzp <sffxzzp@126.com>
2025-09-23 15:32:36 -0600

    try week-4

--- app/page.js
+++ app/page.js
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Go to Week 2 -&gt;</Link>
-      <Link href="/week-3">Go to Week 3 -&gt;</Link>
+      <Link href="/week-2">Go to Week 2 -&gt;</Link><br />
+      <Link href="/week-3">Go to Week 3 -&gt;</Link><br />
+      <Link href="/week-4">Go to Week 4 -&gt;</Link><br />
     </main>
   );
 }
---
+++ app/week-4/new-item.js
@@ -0,0 +1,20 @@
+"use client";
+import { useState } from 'react';
+
+export default function NewItem() {
+	const [quantity, setQuantity] = useState(1);
+
+	const increment = () => {if (quantity < 20) {setQuantity(quantity + 1)}};
+	const decrement = () => {if (quantity > 1) {setQuantity(quantity - 1)}};
+
+	return (
+		<div className="flex flex-col items-center gap-4 p-6 rounded-lg w-full max-w-xs mx-auto border border-gray-300">
+			<div className="text-lg font-semibold mb-2">Quantity: <span className="text-blue-600">{quantity}</span></div>
+			<div className="flex items-center gap-6">
+				<button id="decrement-button" disabled={quantity <= 1} onClick={decrement} className="w-10 h-10 flex items-center justify-center rounded-full border border-gray-300 text-white-700 font-bold text-xl transition hover:border-blue-500 hover:text-blue-600 disabled:opacity-50 disabled:cursor-not-allowed">-</button>
+				<button id="increment-button" onClick={increment} disabled={quantity >= 20} className="w-10 h-10 flex items-center justify-center rounded-full border border-gray-300 text-white-700 font-bold text-xl transition hover:border-blue-500 hover:text-blue-600 disabled:opacity-50 disabled:cursor-not-allowed">+</button>
+			</div>
+			<div className="text-sm text-gray-500 mt-2">Allowed range: <span className="font-semibold text-gray-700">1 - 20</span></div>
+		</div>
+	);
+}
