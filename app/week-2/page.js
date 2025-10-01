428d0b738a605897d72981250c033dde4be42109
sffxzzp <sffxzzp@126.com>
2025-10-01 15:09:57 -0600

    week-2: use a main for page root. and add another header for student info

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -2,9 +2,9 @@ import StudentInfo from "./student-info";
 
 export default function Page() {
   return (
-    <section>
+    <main>
       <h1>Shopping List</h1>
       <StudentInfo />
-    </section>
+    </main>
   );
 }
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function StudentInfo() {
   return (
 	<section>
+	  <h2>Student Information</h2>
 	  <p>Name: Zhiping</p>
 	  <p>Github: <Link href="https://github.com/sffxzzp/cprg306-assignments">sffxzzp/cprg306-assignments</Link></p>
 	</section>
