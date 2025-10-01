a4be919ba82586bc336af66b197a8ecefa93d4e0
sffxzzp <sffxzzp@126.com>
2025-10-01 15:10:35 -0600

    week-4: change some styles

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -9,12 +9,12 @@ export default function NewItem() {
 
 	return (
 		<div className="flex flex-col items-center gap-4 p-6 rounded-lg w-full max-w-xs mx-auto border border-gray-300">
-			<div className="text-lg font-semibold mb-2">Quantity: <span className="text-blue-600">{quantity}</span></div>
+			<div className="text-lg font-bold mb-2">Quantity: <span className="text-cyan-700">{quantity}</span></div>
 			<div className="flex items-center gap-6">
-				<button id="decrement-button" disabled={quantity <= 1} onClick={decrement} className="w-10 h-10 flex items-center justify-center rounded-full border border-gray-300 text-white-700 font-bold text-xl transition hover:border-blue-500 hover:text-blue-600 disabled:opacity-50 disabled:cursor-not-allowed">-</button>
-				<button id="increment-button" onClick={increment} disabled={quantity >= 20} className="w-10 h-10 flex items-center justify-center rounded-full border border-gray-300 text-white-700 font-bold text-xl transition hover:border-blue-500 hover:text-blue-600 disabled:opacity-50 disabled:cursor-not-allowed">+</button>
+				<button id="decrement-button" disabled={quantity <= 1} onClick={decrement} className="w-10 h-10 flex items-center justify-center rounded-full border border-gray-300 text-white-700 transition hover:border-blue-500 hover:text-blue-600 hover:cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed">-</button>
+				<button id="increment-button" onClick={increment} disabled={quantity >= 20} className="w-10 h-10 flex items-center justify-center rounded-full border border-gray-300 text-white-700 transition hover:border-blue-500 hover:text-blue-600 hover:cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed">+</button>
 			</div>
-			<div className="text-sm text-gray-500 mt-2">Allowed range: <span className="font-semibold text-gray-700">1 - 20</span></div>
+			<div className="text-sm text-white-500">Allowed range: <span className="font-bold text-cyan-600">1 - 20</span></div>
 		</div>
 	);
 }
