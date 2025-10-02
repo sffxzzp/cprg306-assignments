49684d46608af7e6cfb6fa02751a691bd1c5dcdc
sffxzzp <sffxzzp@126.com>
2025-10-02 15:53:09 -0600

    week-5: done

--- app/week-5/page.js
+++ app/week-5/page.js
@@ -1,10 +1,10 @@
-import NewItems from './new-item';
+import NewItem from './new-item';
 
 export default function Page() {
 	return (
 		<main className="mx-auto my-4 p-4 max-w-2xl flex flex-col justify-center">
 			<h1 className="mb-4 text-2xl font-bold text-center">Add New Item</h1>
-			<NewItems />
+			<NewItem />
 		</main>
   	);
 }
