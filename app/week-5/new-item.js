440ef34f5829ad3c90f0d1db61580609823e1124
sffxzzp <sffxzzp@126.com>
2025-10-21 16:08:33 -0600

    week-5: higher contrast

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -24,12 +24,12 @@ export default function NewItem() {
 			<section className="flex flex-col gap-4 p-6 rounded-lg w-full max-w-xs mx-auto border border-gray-300">
 				<div className="text-lg font-bold mb-2">Name:</div>
 				<input type="text" placeholder="e.g., milk, 4 L 🥛" value={name} onChange={(e) => setName(e.target.value)} className="border border-gray-300 p-2 rounded" required />
-				<div className="text-lg font-bold mb-2">Quantity: <span className="text-cyan-700">{quantity}</span></div>
+				<div className="text-lg font-bold mb-2">Quantity: <span className="text-cyan-300">{quantity}</span></div>
 				<div className="flex gap-6">
 					<button type="button" id="decrement-button" disabled={quantity <= 1} onClick={decrement} className="w-10 h-10 flex items-center justify-center rounded-full border border-gray-300 text-white-700 transition hover:border-blue-500 hover:text-blue-600 hover:cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed">-</button>
 					<button type="button" id="increment-button" disabled={quantity >= 20} onClick={increment} className="w-10 h-10 flex items-center justify-center rounded-full border border-gray-300 text-white-700 transition hover:border-blue-500 hover:text-blue-600 hover:cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed">+</button>
 				</div>
-				<div className="text-sm text-white-500">Allowed range: <span className="font-bold text-cyan-600">1 - 20</span></div>
+				<div className="text-sm text-white-500">Allowed range: <span className="font-bold text-cyan-300">1 - 20</span></div>
 				<div className="text-lg font-bold mb-2">Category:</div>
 				<select value={category} onChange={(e) => setCategory(e.target.value)} className="border border-gray-300 p-2 rounded">
 					<option value="produce">Produce</option>
