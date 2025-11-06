bff46b2b8b43004f3afe960de1e6ae31da8057da
sffxzzp <sffxzzp@126.com>
2025-11-06 16:45:52 -0700

    week-9: cursor style when hovering

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -10,13 +10,13 @@ export default function Page() {
 			{user ? (
 				<div>
 					<p>Welcome, {user.displayName} ({user.email})</p>
-					<p><button onClick={() => router.push("/week-9/shopping-list")}>Go to Shopping List</button></p>
-					<p><button onClick={firebaseSignOut}>Sign Out</button></p>
+					<p><button className="hover:cursor-pointer" onClick={() => router.push("/week-9/shopping-list")}>Go to Shopping List</button></p>
+					<p><button className="hover:cursor-pointer" onClick={firebaseSignOut}>Sign Out</button></p>
 				</div>
 			) : (
 				<div>
 					<p>Please sign in.</p>
-					<p><button onClick={gitHubSignIn}>Sign In with GitHub</button></p>
+					<p><button className="hover:cursor-pointer" onClick={gitHubSignIn}>Sign In with GitHub</button></p>
 				</div>
 			)}
 		</main>	
