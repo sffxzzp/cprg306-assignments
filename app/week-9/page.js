0c5008fe92a34e9b550991d41a39169af2753c63
sffxzzp <sffxzzp@126.com>
2025-11-09 17:43:45 -0700

    week-9: styling login page

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,24 +1,28 @@
 "use client";
+import Link from "next/link";
 import { useUserAuth } from "../contexts/AuthContext";
-import { useRouter } from "next/navigation";
 
 export default function Page() {
-	const router = useRouter();
 	const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
 	return (
-		<main>
-			{user ? (
-				<div>
-					<p>Welcome, {user.displayName} ({user.email})</p>
-					<p><button className="hover:cursor-pointer" onClick={() => router.push("/week-9/shopping-list")}>Go to Shopping List</button></p>
-					<p><button className="hover:cursor-pointer" onClick={firebaseSignOut}>Sign Out</button></p>
-				</div>
-			) : (
-				<div>
-					<p>Please sign in.</p>
-					<p><button className="hover:cursor-pointer" onClick={gitHubSignIn}>Sign In with GitHub</button></p>
-				</div>
-			)}
+		<main className="mx-auto my-6 p-4 max-w-xl">
+			<div className="rounded-xl border p-6 shadow-sm">
+				<h1 className="text-2xl font-bold text-center mb-4">Shopping List</h1>
+				{user ? (
+					<div className="space-y-4 text-center">
+						<p>Welcome, <span className="font-bold">{user.displayName}</span> (<span className="text-cyan-600 dark:text-cyan-400">{user.email}</span>)</p>
+						<div className="flex flex-col sm:flex-row gap-3 justify-center">
+							<Link className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition hover:cursor-pointer" href="/week-9/shopping-list">Go to Shopping List</Link>
+							<button onClick={firebaseSignOut} className="border py-2 px-4 rounded hover:border-blue-800 hover:text-blue-600 hover:cursor-pointer transition">Sign Out</button>
+						</div>
+					</div>
+				) : (
+					<div className="space-y-4 text-center">
+						<p>Please sign in.</p>
+						<button onClick={gitHubSignIn} className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition hover:cursor-pointer">Sign In with GitHub</button>
+					</div>
+				)}
+			</div>
 		</main>	
 	);
 }
