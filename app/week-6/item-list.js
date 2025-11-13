5ab6203f2da1d638a0278e4a44b393eb0c22e9e1
sffxzzp <sffxzzp@126.com>
2025-11-12 19:58:25 -0700

    week-6 to week-9: better sortBy & hover states on buttons

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -8,16 +8,18 @@ export default function ItemList() {
 	const items = [...itemsData].sort((a, b) => {
 		if (sortBy === 'name') {
 			return a.name.localeCompare(b.name);
-		} else {
+		} else if (sortBy === 'category') {
 			return a.category.localeCompare(b.category);
+		} else {
+			return 0;
 		}
 	});
 	return (
 		<section>
 			<div className="flex items-center gap-2">
-				Sort by: 
-				<button onClick={() => setSortBy('name')} className={`px-2 py-2 rounded ${sortBy === 'name' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'}`}>Name</button>
-				<button onClick={() => setSortBy('category')} className={`px-2 py-2 rounded ${sortBy === 'category' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'}`}>Category</button>
+				<span>Sort by: </span>
+				<button onClick={() => setSortBy('name')} className={`px-2 py-2 rounded ${sortBy === 'name' ? 'hover:bg-blue-600 bg-blue-500 text-white' : 'hover:bg-blue-100 bg-gray-200 text-black'}`}>Name</button>
+				<button onClick={() => setSortBy('category')} className={`px-2 py-2 rounded ${sortBy === 'category' ? 'hover:bg-blue-600 bg-blue-500 text-white' : 'hover:bg-blue-100 bg-gray-200 text-black'}`}>Category</button>
 			</div>
 			<ul>
 				{items.map(item => (
--- app/week-8/item-list.js
+++ app/week-8/item-list.js
@@ -7,16 +7,18 @@ export default function ItemList({ items, onItemSelect }) {
 	const sortedItems = [...items].sort((a, b) => {
 		if (sortBy === 'name') {
 			return a.name.localeCompare(b.name);
-		} else {
+		} else if (sortBy === 'category') {
 			return a.category.localeCompare(b.category);
+		} else {
+			return 0;
 		}
 	});
 	return (
 		<section>
 			<div className="flex items-center gap-2">
 				Sort by: 
-				<button onClick={() => setSortBy('name')} className={`px-2 py-2 rounded ${sortBy === 'name' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'}`}>Name</button>
-				<button onClick={() => setSortBy('category')} className={`px-2 py-2 rounded ${sortBy === 'category' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'}`}>Category</button>
+				<button onClick={() => setSortBy('name')} className={`px-2 py-2 rounded ${sortBy === 'name' ? 'hover:bg-blue-600 bg-blue-500 text-white' : 'hover:bg-blue-100 bg-gray-200 text-black'}`}>Name</button>
+				<button onClick={() => setSortBy('category')} className={`px-2 py-2 rounded ${sortBy === 'category' ? 'hover:bg-blue-600 bg-blue-500 text-white' : 'hover:bg-blue-100 bg-gray-200 text-black'}`}>Category</button>
 			</div>
 			<ul>
 				{sortedItems.map(item => (
