eecc64843c021ee6d4778cb804cd525e3171936a
sffxzzp <sffxzzp@126.com>
2025-11-24 15:12:25 -0700

    week-10: styling delete button

--- app/week-10/shopping-list/item.js
+++ app/week-10/shopping-list/item.js
@@ -3,7 +3,7 @@ export default function Item({ name, quantity, category, onSelect, onDelete }) {
 		<li className="rounded-xl border p-5 my-4 shadow-sm dark:text-white dark:hover:bg-white hover:text-cyan-600 dark:hover:text-gray-800 hover:cursor-pointer" onClick={() => onSelect(name)}>
 			<div className="flex flex-row justify-between items-center">
 				<span className="font-bold text-lg">{name}</span>
-				<button className="text-red-500 hover:text-red-700 hover:cursor-pointer" onClick={(e) => { e.stopPropagation(); onDelete(); }}>Delete</button>
+				<button className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-700 hover:cursor-pointer" onClick={(e) => { e.stopPropagation(); onDelete(); }}>Delete</button>
 			</div>
 			<div className="flex flex-row justify-between items-center">
 				<span>Quantity: <span className="text-blue-400">{quantity}</span></span>
